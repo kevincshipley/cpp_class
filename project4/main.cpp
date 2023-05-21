19f90faa0c3eb28ad80ea67ee8d23ed0cfac3690
kevin8pizza <apeesque@gmail.com>
2023-05-21 15:44:45 -0700

    adding Kevin's order to main
    having issues with DeliveryOrder pointer still. Building out try blocks for first boba order

--- project4/main.cpp
+++ project4/main.cpp
@@ -39,7 +39,61 @@ int main () {
     Account bob("Bob");
 
     // We will then have a DeliveryOrder pointer that we will later use to point to different orders to allow polymorphism.
-    DeliveryOrder* order;
+    DeliveryOrder* order; // DeliveryOrder base class pointer
+
+    // 1. Kevin Placing Order
+    // Kevin starts to order boba drinks for everyone. Let's first output
+    cout << "Kevin placing order.\n";
+    // He creates his order with his name "Kevin", date "11/20/2022", phone number "123-456-0000". He's placing order at the shop "M Tea" and looks like the miles for the delivery is 10.4 miles.
+
+    // BobaOrder kOrder("Kevin", "11/20/2022", "123-456-0000", 10.4, "M Tea");
+    // order = &kOrder; // DeliveryOrder pointer points to BobaOrder object ???
+
+    order.BobaOrder("Kevin", "11/20/2022", "123-456-0000", 10.4, "M Tea");
+
+    // Because each of the addDrink() function could cause exception, let's put all the addDrink() function calls in a try catch block. Catch the InvalidInput exception by reference and trigger the print_reason() function of the exception.
+    try {
+        order.addDrink("Green Tea Latte"); // Kevin adds his first drink order "Green Tea Latte" with all default choices for this drink
+    }
+    catch (InvalidInput &) {
+        throw InvalidInput("Green Tea Latte");
+        cout << "Not serving requested drinks. Drink order ignored.\n\n";
+    }
+    try {
+        order.addDrink("Brown Sugar Pearl Milk", 0); // then his second drink "Brown Sugar Pearl Milk" with no boba
+    }
+    catch (InvalidInput &) {
+        throw InvalidInput("Brown Sugar Pearl Milk");
+        cout << "Not serving requested drinks. Drink order ignored.\n\n";
+    }
+    try {
+        order.addDrink("Brown Sugar Boba Milk", 0, 2); // and two more "Brown Sugar Boba Milk" with no boba.
+    }
+    catch (InvalidInput &) {
+        throw InvalidInput("Brown Sugar Boba Milk");
+        cout << "Not serving requested drinks. Drink order ignored.\n\n";
+    }
+    try {
+        order.addDrink("Iron Goddess"); // Kevin added another order "Iron Goddess" without knowing that the shop isn't serving this drink anymore. This should return an error message for him.
+    }
+    catch (InvalidInput &) {
+        throw InvalidInput("Iron Goddess");
+        cout << "Not serving requested drinks. Drink order ignored.\n\n";
+    }
+
+    // Now the system should print out his receipt.
+    // We first need to call receipt() function to print out the receipt.
+    // Make sure you set cout to only print out 2 decimal places.
+    order.receipt();
+    // Then we output the delivery balance by calling the getTotalBalance() function.
+    // The format should be similar to "Balance: $99.99\n".
+    
+    // Don't forget the dollar sign. 
+    // Then we want to output the discounted balance like this "Discounted Balance: $88.88\n".
+    // We get the discounted balance by having our DeliveryOrder pointer points to Kevin's order and trigger the applyDiscount() function with Kevin's account.
+    // Finally we finished the receipt by having two addition new lines. (Detail output format see the expected output below)
+
+
 
     return 0;
 }
@@ -51,14 +105,14 @@ float applyDiscount(DeliveryOrder ordr, Account& acct) // take in a DeliveryOrde
     // Check the account status to determine how to apply the discount
     if (aStatus == "Owner") // Owner
     {
-        return 0.1 * ordr.getOrderBalance(); // return the discounted balance of the order
+        return 0.1 * ordr.getOrderBalance(); // Owner get 90% off orders
     }
     else if (aStatus == "VIP") // VIP
     {
-        return ordr.VIPdiscount() * ordr.getOrderBalance(); 
+        return ordr.VIPdiscount() * ordr.getOrderBalance(); // return the order discounted based on Boba/Food order discount
     }
     else if (aStatus == "Regular") // Regular
     {
-        return ordr.getOrderBalance();
+        return ordr.getOrderBalance(); // no modification to order for Regular users
     }
 };
